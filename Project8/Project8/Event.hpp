2a2bef48a318da431578e84d249532591119f868
NidoKota <nido.kota@gmail.com>
2024-01-29 02:05:39 +0900

    Event::Subscribeの右辺値への対応

--- Project8/Project8/Event.hpp
+++ Project8/Project8/Event.hpp
@@ -13,6 +13,7 @@ class IReadOnlyEvent
 
 public:
 	inline virtual void Subscribe(FuncType& func) = 0;
+	inline virtual FuncType& Subscribe(FuncType&& func) = 0;
 	inline virtual void Unsubscribe(FuncType& func) = 0;
 	inline ~IReadOnlyEvent() {}
 };
@@ -23,26 +24,34 @@ class Event : public IReadOnlyEvent<TOutput, TInput>
 	using FuncType = function<TOutput(TInput)>;
 
 private:
-	vector<reference_wrapper<FuncType>> _funcs = vector<reference_wrapper<FuncType>>();
+	vector<FuncType> _funcs = vector<FuncType>();
+	vector<reference_wrapper<FuncType>> _funcRefs = vector<reference_wrapper<FuncType>>();
 
 public:
 	inline void Subscribe(FuncType& func) override
 	{
+		_funcRefs.push_back(func);
+	}
+
+	inline FuncType& Subscribe(FuncType&& func) override
+	{
 		_funcs.push_back(func);
+		_funcRefs.push_back(_funcs.back());
+		return _funcs.back();
 	}
 
 	inline void Unsubscribe(FuncType& func) override
 	{
-		auto rm = remove_if(begin(_funcs), end(_funcs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
-		_funcs.erase(rm, cend(_funcs));
+		auto rm = remove_if(begin(_funcRefs), end(_funcRefs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
+		_funcRefs.erase(rm, cend(_funcRefs));
 	}
 
 	inline vector<TOutput> operator()(TInput inputs) const
 	{
-		vector<TOutput> result = vector<TOutput>(_funcs.size());
-		for (size_t i = 0; i < _funcs.size(); i++)
+		vector<TOutput> result = vector<TOutput>(_funcRefs.size());
+		for (size_t i = 0; i < _funcRefs.size(); i++)
 		{
-			const FuncType& func = _funcs[i].get();
+			const FuncType& func = _funcRefs[i].get();
 			result[i] = func(inputs);
 		}
 		return result;
@@ -55,25 +64,33 @@ class Event<void, TInput> : public IReadOnlyEvent<void, TInput>
 	using FuncType = function<void(TInput)>;
 
 private:
-	vector<reference_wrapper<FuncType>> _funcs = vector<reference_wrapper<FuncType>>();
+	vector<FuncType> _funcs = vector<FuncType>();
+	vector<reference_wrapper<FuncType>> _funcRefs = vector<reference_wrapper<FuncType>>();
 
 public:
 	inline void Subscribe(FuncType& func) override
 	{
+		_funcRefs.push_back(func);
+	}
+
+	inline FuncType& Subscribe(FuncType&& func) override
+	{
 		_funcs.push_back(func);
+		_funcRefs.push_back(_funcs.back());
+		return _funcs.back();
 	}
 
 	inline void Unsubscribe(FuncType& func) override
 	{
-		auto rm = remove_if(begin(_funcs), end(_funcs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
-		_funcs.erase(rm, cend(_funcs));
+		auto rm = remove_if(begin(_funcRefs), end(_funcRefs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
+		_funcRefs.erase(rm, cend(_funcRefs));
 	}
 
 	inline void operator()(TInput inputs) const
 	{
-		for (size_t i = 0; i < _funcs.size(); i++)
+		for (size_t i = 0; i < _funcRefs.size(); i++)
 		{
-			const FuncType& func = _funcs[i].get();
+			const FuncType& func = _funcRefs[i].get();
 			func(inputs);
 		}
 	}
@@ -85,26 +102,34 @@ class Event<TOutput, void> : public IReadOnlyEvent<TOutput, void>
 	using FuncType = function<TOutput(void)>;
 
 private:
-	vector<reference_wrapper<FuncType>> _funcs = vector<reference_wrapper<FuncType>>();
+	vector<FuncType> _funcs = vector<FuncType>();
+	vector<reference_wrapper<FuncType>> _funcRefs = vector<reference_wrapper<FuncType>>();
 
 public:
 	inline void Subscribe(FuncType& func) override
 	{
+		_funcRefs.push_back(func);
+	}
+
+	inline FuncType& Subscribe(FuncType&& func) override
+	{
 		_funcs.push_back(func);
+		_funcRefs.push_back(_funcs.back());
+		return _funcs.back();
 	}
 
 	inline void Unsubscribe(FuncType& func) override
 	{
-		auto rm = remove_if(begin(_funcs), end(_funcs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
-		_funcs.erase(rm, cend(_funcs));
+		auto rm = remove_if(begin(_funcRefs), end(_funcRefs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
+		_funcRefs.erase(rm, cend(_funcRefs));
 	}
 
 	inline vector<TOutput> operator()() const
 	{
-		vector<TOutput> result = vector<TOutput>(_funcs.size());
-		for (size_t i = 0; i < _funcs.size(); i++)
+		vector<TOutput> result = vector<TOutput>(_funcRefs.size());
+		for (size_t i = 0; i < _funcRefs.size(); i++)
 		{
-			const FuncType& func = _funcs[i].get();
+			const FuncType& func = _funcRefs[i].get();
 			result[i] = func();
 		}
 		return result;
@@ -117,25 +142,33 @@ class Event<void, void> : public IReadOnlyEvent<void, void>
 	using FuncType = function<void(void)>;
 
 private:
-	vector<reference_wrapper<FuncType>> _funcs = vector<reference_wrapper<FuncType>>();
+	vector<FuncType> _funcs = vector<FuncType>();
+	vector<reference_wrapper<FuncType>> _funcRefs = vector<reference_wrapper<FuncType>>();
 
 public:
 	inline void Subscribe(FuncType& func) override
 	{
+		_funcRefs.push_back(func);
+	}
+
+	inline FuncType& Subscribe(FuncType&& func) override
+	{
 		_funcs.push_back(func);
+		_funcRefs.push_back(_funcs.back());
+		return _funcs.back();
 	}
 
 	inline void Unsubscribe(FuncType& func) override
 	{
-		auto rm = remove_if(begin(_funcs), end(_funcs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
-		_funcs.erase(rm, cend(_funcs));
+		auto rm = remove_if(begin(_funcRefs), end(_funcRefs), [&func](reference_wrapper<FuncType> f) { return &func == &f.get(); });
+		_funcRefs.erase(rm, cend(_funcRefs));
 	}
 
 	inline void operator()() const
 	{
-		for (size_t i = 0; i < _funcs.size(); i++)
+		for (size_t i = 0; i < _funcRefs.size(); i++)
 		{
-			const FuncType& func = _funcs[i].get();
+			const FuncType& func = _funcRefs[i].get();
 			func();
 		}
 	}
